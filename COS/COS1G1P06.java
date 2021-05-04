04a189d514246c97aba57a69da41e7984c3701ce
Kim-SuBin <sb2504@naver.com>
2021-05-04 16:25:54 +0900

    [Solve] ✍ COS 1급 JAVA Goorm 1차

---
+++ COS/COS1G1P06.java
@@ -0,0 +1,29 @@
+package COS;
+
+// [1차] 체스의 나이트
+class COS1G1P06 {
+    public int solution(String pos) {
+        int[] dr = {-2, -2, -1, 1, 2, 2, 1, -1};
+        int[] dc = {-1, 1, 2, 2, 1, -1, -2, -2};
+        int answer = 0;
+        int curR = Integer.valueOf(pos.toCharArray()[0] - 'A');
+        int curC = 8 - Integer.valueOf(pos.toCharArray()[1] - '0');
+        int nr, nc;
+        for (int i = 0; i < 8; i++) {
+            nr = curR + dr[i];
+            nc = curC + dc[i];
+            if (0 <= nr && nr < 8 && 0 <= nc && nc < 8) answer += 1;
+        }
+        return answer;
+    }
+
+    // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
+    public static void main(String[] args) {
+        COS1G1P06 sol = new COS1G1P06();
+        String pos = "A7";
+        int ret = sol.solution(pos);
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
+    }
+}
