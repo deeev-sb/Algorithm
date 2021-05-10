a53a09e74aa9563ae89167252e1c91372c0edd90
Kim-SuBin <sb2504@naver.com>
2021-05-10 23:57:50 +0900

    [Solve] ✍ 나이순 정렬

---
+++ Baekjoon/Silver/B10814.java
@@ -0,0 +1,38 @@
+package Baekjoon.Silver;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class B10814 { // 나이순 정렬
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int num = Integer.parseInt(br.readLine());
+        String[][] member = new String[num][2];
+
+        for (int i = 0; i < num; i++) {
+            st = new StringTokenizer(br.readLine());
+            member[i][0] = st.nextToken();
+            member[i][1] = st.nextToken();
+        }
+
+        Arrays.sort(member, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                return Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]);
+            }
+        });
+
+        for (int i = 0; i < num; i++) {
+            bw.write(member[i][0] + " " + member[i][1] + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
