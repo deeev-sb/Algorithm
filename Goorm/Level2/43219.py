d2397a69e83ea0ef8c953a0b5b8c8b591baf2d8d
Subin Kim <sb2504@naver.com>
2021-05-28 15:02:27 +0900

    [Solve] ✍ 문자열 뒤집기

---
+++ Goorm/Level2/43219.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+
+def reverse_text(text) :
+	return text[::-1]
+
+if __name__=="__main__" :
+	text = input()
+	print(reverse_text(text))
+	
