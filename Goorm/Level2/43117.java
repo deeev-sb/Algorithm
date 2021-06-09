6a3dbbdc26625ed71eb954be3c6766ee123d2712
Subin Kim <sb2504@naver.com>
2021-06-09 17:55:22 +0900

    [Solve] ✍ 절댓값

---
+++ Goorm/Level2/43117.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+// 절댓값
+class G43117 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int input = Integer.parseInt(br.readLine());
+		System.out.println(Math.abs(input));
+	}
+}
