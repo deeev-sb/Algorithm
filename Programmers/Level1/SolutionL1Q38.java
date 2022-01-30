38a826a2398cc0b5c4c88d88974e8c229edd54b3
Subin Kim <sb2504@naver.com>
2022-01-30 16:59:38 +0900

    [Solve] ✍ 소수 만들기

---
+++ Programmers/Level1/SolutionL1Q38.java
@@ -0,0 +1,32 @@
+// 소수 만들기
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int numsLen = nums.length;
+        for (int i = 0; i < numsLen; i++) {
+            for (int j = i + 1; j < numsLen; j++) {
+                for (int k = j + 1; k < numsLen; k++){
+                    if (isPrime(nums[i] + nums[j] + nums[k])) {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+    
+    public Boolean isPrime(int num) {
+        int check = 1;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                check++;
+            }
+            if (check > 2) {
+                return false;
+            }
+        }
+        return check==1;
+        
+    }
+}
