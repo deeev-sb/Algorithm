077bb5ac79c15f51cf7219e41568e82724ac396f
Kim SuBin <sb2504@naver.com>
2022-03-02 23:47:14 +0900

    [Solve] ✍ 단어 뒤집기

---
+++ Java_Algorithm/String/String04.java
@@ -0,0 +1,22 @@
+package String;
+
+import java.util.Scanner;
+
+// 단어 뒤집기
+// N개의 단어를 입력된 순서대로 한 줄에 하나씩 뒤집어서 출력
+// good -> doog
+// Time -> emiT
+public class String04 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        StringBuilder stringBuilder;
+
+        int testcase = input.nextInt();
+
+        for (int i = 0; i < testcase; i++) {
+            stringBuilder = new StringBuilder(input.next());
+            stringBuilder.reverse();
+            System.out.println(stringBuilder);
+        }
+    }
+}
