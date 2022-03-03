c6ffc8d51b23242b67e73839bcafe72ebd160b4d
Kim SuBin <sb2504@naver.com>
2022-03-03 22:10:51 +0900

    [Solve] ✍ 특정 문자 뒤집기

---
+++ Java_Algorithm/String/String05.java
@@ -0,0 +1,29 @@
+package String;
+
+import java.util.Scanner;
+
+// 특정 문자 뒤집기
+// 알파벳만 뒤집힌 문자열 출력
+// a#b!GE*T@S -> S#T!EG*b@a
+public class String05 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String text = input.next();
+
+        StringBuilder alpha = new StringBuilder(text.replaceAll("[^a-zA-z]", "")).reverse();
+        StringBuilder answer = new StringBuilder(text.replaceAll("[a-zA-z]", "0"));
+
+
+        int idx = 0;
+        char str;
+        for (int i = 0; i < answer.length(); i++) {
+            str = answer.charAt(i);
+            if (Character.compare(str, '0') == 0) {
+                answer.replace(i, i+1, String.valueOf(alpha.charAt(idx++)));
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
+
