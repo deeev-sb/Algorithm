cd7535ef2fe1acc7e8c7af97ca655f2f33e36d37
deeev-sb <sb2504@naver.com>
2025-01-01 23:26:22 +0900

    [Solve] ✍️ #programmers 코딩 기초 트레이닝 day01
    - 문자열 출력하기
    - a와 b 출력하기
    - 문자열 반복해서 출력하기

---
+++ programmers/foundation_kotlinPractice/day01_01 문자열 출력하기.kt
@@ -0,0 +1,8 @@
+package foundation_kotlinPractice
+
+// 문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요.
+
+fun main(args: Array<String>) {
+    val s1 = readLine()!!
+    print(s1)
+}
---
+++ programmers/foundation_kotlinPractice/day01_03 문자열 반복해서 출력하기.kt
@@ -0,0 +1,14 @@
+package foundation_kotlinPractice
+
+//문자열 str과 정수 n이 주어집니다.
+//str이 n번 반복된 문자열을 만들어 출력하는 코드를 작성해 보세요.
+
+fun main(args: Array<String>) {
+    val input = readLine()!!.split(' ')
+    val s1 = input[0]
+    val a = input[1]!!.toInt()
+
+    for (i in 1..a) {
+        print(s1)
+    }
+}
